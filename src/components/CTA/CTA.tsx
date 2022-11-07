8ea3e4d1af69a631b1f28722459d35bc2791c2a9
Samuel Carvalho <93744509+samuelcarvalhodeveloper@users.noreply.github.com>
2022-11-07 17:39:10 -0300

    Update CTA.tsx

--- src/components/CTA/CTA.tsx
+++ src/components/CTA/CTA.tsx
@@ -4,13 +4,13 @@ import FacebookLogo from "../../assets/img/FacebookLogo.svg";
 function CTA() {
   return (
     <CTAStyled>
-      <h1 className="cta__title">
+      <div className="cta__title">
         <img
           src={FacebookLogo}
           alt="FacebookL Logo"
           className="cta__logo"
         />
-      </h1>
+      </div>
       <h2 className="cta__label">
         Connect with friends and the world around you on Facebook.
       </h2>
